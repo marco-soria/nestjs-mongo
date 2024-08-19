c0ec3aa702be5d76b8218da97336d221ea8f9f19
marco-soria <marco.soriaa7@gmail.com>
2024-08-19 09:17:22 -0500

    initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module, HttpModule, HttpService } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
@@ -7,17 +8,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-import { enviroments } from './enviroments';
+import { environments } from './environments';
 import config from './config';
+import { lastValueFrom } from 'rxjs';
 
+console.log('API_KEY:', process.env.API_KEY);
+console.log('DATABASE_NAME:', process.env.DATABASE_NAME);
+console.log('DATABASE_PORT:', process.env.DATABASE_PORT);
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.number().required(),
+        API_KEY: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
       }),
@@ -33,13 +38,16 @@ import config from './config';
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
-        const tasks = await http
-          .get('https://jsonplaceholder.typicode.com/todos')
-          .toPromise();
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
         return tasks.data;
       },
       inject: [HttpService],
     },
   ],
 })
+
+
 export class AppModule {}
+
+
